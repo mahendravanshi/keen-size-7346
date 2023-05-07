b5c3eec15f8dd6e9c97863ac2c8cc9f23c3dbc1b
Mahendra <mahendrac121@gmail.com>
2023-05-07 19:44:17 +0530

    added day-5

--- src/com/masai/exception/BusException.java
+++ src/com/masai/exception/BusException.java
@@ -1,11 +1,36 @@
 package com.masai.exception;
 
-public class BusException {
+public class BusException  extends Exception{
 
 	public BusException() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public BusException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
 
 	
 	
